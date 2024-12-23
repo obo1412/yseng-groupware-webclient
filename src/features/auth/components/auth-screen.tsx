23d715bb7ea78d101c7bb62b489b4efd242342fe
obo1412 <obo1412@gmail.com>
2024-12-23 21:52:06 +0900

    init

---
+++ src/features/auth/components/auth-screen.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+import { SignInFlow } from "../types";
+
+export const AuthScreen = () => {
+  const [state, setState] = useState<SignInFlow>("signIn");
+  return (
+    <div className="h-full flex items-center justify-center bg-[#5c3b58]">
+      Auth Screenasdfadfdfdfdfsdf
+    </div>
+  );
+};
