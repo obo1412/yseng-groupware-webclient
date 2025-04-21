31f3c66a54d5d6bb025443d07eff5dabed4f1e60
obo1412 <obo1412@gmail.com>
2025-04-21 23:58:37 +0900

    middleware 접근처리
    로그인 상태에 따른 /인덱스 페이지와 /auth 페이지 처리

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,19 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const response = NextResponse.next();
 
-  if (
-    !req.cookies.get("refreshToken") ||
-    req.nextUrl.pathname.startsWith("/protected")
-  ) {
-    console.log("no refreshToken");
+  if (!req.cookies.get("refreshToken") && req.nextUrl.pathname === "/") {
+    // console.log("path === /");
     return NextResponse.redirect(new URL("/auth", req.url));
+  }
+
+  if (req.cookies.get("refreshToken") && req.nextUrl.pathname === "/auth") {
+    // console.log("move to /");
+    return NextResponse.redirect(new URL("/", req.url));
   }
   return response;
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  // matcher: ["/|/auth/:path*"],
 };
